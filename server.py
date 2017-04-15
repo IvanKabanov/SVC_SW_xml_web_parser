384fd2b3f8575f3e6c5f857d86001857c9cb3a91
Ivan Kabanov <ivan.i.kabanov@gmail.com>
2017-04-15 12:47:23 +0300

    added base server functionality, site templates

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from random import randint
+from werkzeug import secure_filename
+import os
+
+svc_web_parser = Flask(__name__)
+
+svc_web_parser.config['UPLOAD_FOLDER'] = 'uploads/'
+
+@svc_web_parser.route('/')
+def index():
+    return render_template('index.html')
+
+
+@svc_web_parser.route('/allstats')
+def allstats():
+    return render_template('allstats.html')    
+
+
+@svc_web_parser.route('/<upload_id>')
+def stats_visualization_by_id(upload_id):
+    return render_template('results.html')
+
+
+@svc_web_parser.route('/upload', methods=['POST'])
+def upload():
+    upload_id = randint(100000, 999999)
+    #!!!!Don't forget to check if upload_id exists!!!!
+    uploaded_files = request.files.getlist("file[]")
+    print(uploaded_files)
+    filenames = []
+    for file in uploaded_files:
+        filename = secure_filename(file.filename) #Фиг знает зачем, но пишут, что надо (удаляет нечитаемые символы из имени)
+        file.save(os.path.join(svc_web_parser.config['UPLOAD_FOLDER'], filename))
+        filename = svc_web_parser.config['UPLOAD_FOLDER'] + filename
+        filenames.append(filename)
+    print(filenames)
+    return render_template('upload.html', upload_id = upload_id)
+
+if __name__ == "__main__":
+    svc_web_parser.run(debug=True)
+
+
